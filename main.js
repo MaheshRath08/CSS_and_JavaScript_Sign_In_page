67feb400e5ea93bd5bfe8901655b39d30961e71d
MaheshRath08 <maheshrath.2002@gmail.com>
2022-11-12 23:31:46 +0530

    fixed bugs

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let show = document.getElementById("show")
-let pass = document.getElementById("pass")
+let pass = document.querySelector(".pass")
 
 show.addEventListener("click", ()=>{
     pass.classList.toggle("showIt")
